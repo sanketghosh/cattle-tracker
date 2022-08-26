0cf0dd2ac131319ef4e075c9b22f66b824556f9a
Sanket <code.sanket13@gmail.com>
2022-08-26 23:58:52 +0530

    adding context

--- frontend/src/context/CattleContext.js
+++ frontend/src/context/CattleContext.js
@@ -2,8 +2,25 @@ import { createContext, useReducer } from "react";
 
 const CattleContext = createContext();
 
+export const cattleReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CATTLE":
+      return {
+        cattle: action.payload,
+      };
+    case "CREATE_CATTLE":
+      return {
+        cattle: [action.payload, ...state.cattle],
+      };
+  }
+};
+
 export function CattleContextProvider({ children }) {
-  const [state, dispatch] = useReducer;
+  const [state, dispatch] = useReducer(cattleReducer, {
+    cattle: null,
+  });
+
+  // dispatch({ type: "SET_CATTLE", payload: [{}, {}] });
 
   return <CattleContext.Provider>{children}</CattleContext.Provider>;
 }
